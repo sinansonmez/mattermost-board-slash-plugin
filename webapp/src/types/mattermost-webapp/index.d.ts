ed0795639a1e23f6088be67a078e4c206e76bc16
Sinan Sonmez (Chaush) <37421564+sinansonmez@users.noreply.github.com>
2022-06-17 18:20:05 +0200

    initial setup

--- webapp/src/types/mattermost-webapp/index.d.ts
+++ webapp/src/types/mattermost-webapp/index.d.ts
@@ -1,5 +1,7 @@
 export interface PluginRegistry {
     registerPostTypeComponent(typeName: string, component: React.ElementType)
+    registerRootComponent(component: JSX.Element)
+    registerMainMenuAction(message: string, action: MainMenuAction, icon: JSX.Element)
 
     // Add more if needed from https://developers.mattermost.com/extend/plugins/webapp/reference
 }
